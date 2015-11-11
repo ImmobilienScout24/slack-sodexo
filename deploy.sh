80fefdc4bf285ea80041a35fcd8ca15702d82084
mriehl <max@riehl.io>
2015-11-11 13:21:19 +0100

    bump ram size (and CPU)

--- deploy.sh
+++ deploy.sh
@@ -16,4 +16,4 @@ aws --region eu-west-1 lambda update-function-code --function-name "$functionNam
     echo "update failed, creating the function"
     aws --region eu-west-1 lambda create-function --timeout 60 --function-name "$functionName" --runtime "java8" --role "arn:aws:iam::285072396330:role/lambda_basic_execution" --handler "de.is24.Sodexo::handleCall" --code 'S3Bucket=sodexo-slack,S3Key=sodexo.jar'
 }
-aws --region eu-west-1 lambda update-function-configuration --function-name "$functionName" --memory-size 256 --timeout 60
+aws --region eu-west-1 lambda update-function-configuration --function-name "$functionName" --memory-size 512 --timeout 60
