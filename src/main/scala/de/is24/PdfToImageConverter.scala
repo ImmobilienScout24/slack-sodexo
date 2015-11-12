8e8cebb28ea623b2eda3a35d8ab764ff67c7aaaa
Patrick Haun <patrick.haun@immobilienscout24.de>
2015-11-12 09:10:48 +0100

    use managed resource

--- src/main/scala/de/is24/PdfToImageConverter.scala
+++ src/main/scala/de/is24/PdfToImageConverter.scala
@@ -4,18 +4,20 @@ import java.awt.image.BufferedImage
 import java.io.ByteArrayInputStream
 
 import org.apache.pdfbox.pdmodel.{PDDocument, PDPage}
+import resource._
 
 object PdfToImageConverter {
 
   def convert(pdfBytes: Array[Byte])(implicit logger: SodexoLogger): BufferedImage = {
 
     logger.log("converting PDF to image")
-    val document = PDDocument.load(new ByteArrayInputStream(pdfBytes))
-    val pages = document.getDocumentCatalog.getAllPages.asInstanceOf[java.util.List[PDPage]]
-    val firstPage = pages.get(0)
-    val image = firstPage.convertToImage(BufferedImage.TYPE_INT_RGB, ImageConstants.RESOLUTION)
-    logger.log("conversion done")
-    image
+    managed(PDDocument.load(new ByteArrayInputStream(pdfBytes))).acquireAndGet { document =>
+      val pages = document.getDocumentCatalog.getAllPages.asInstanceOf[java.util.List[PDPage]]
+      val firstPage = pages.get(0)
+      val image = firstPage.convertToImage(BufferedImage.TYPE_INT_RGB, ImageConstants.RESOLUTION)
+      logger.log("conversion done")
+      image
+    }
   }
 
 }
