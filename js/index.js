89157f3c5cd4ddad23ac4d6d0e1e3bee52041a68
Patrick Haun <patrick.haun@immobilienscout24.de>
2015-11-13 16:29:14 +0100

    add week to image name

--- js/index.js
+++ js/index.js
@@ -1,46 +1,74 @@
 exports.handler = function(event, context) {
-    console.log("got event: " + JSON.stringify(event))
-    var d = new Date()
-    var weekdays = new Array(7)
-    weekdays[0]=  "sunday"
-    weekdays[1] = "monday"
-    weekdays[2] = "tuesday"
-    weekdays[3] = "wednesday"
-    weekdays[4] = "thursday"
-    weekdays[5] = "friday"
-    weekdays[6] = "saturday"
+  console.log("got event: " + JSON.stringify(event))
+  Date.prototype.getWeek = function () {
+    // Create a copy of this date object
+    var target  = new Date(this.valueOf());
 
-    var weekday = ""
-    if(event.text !== undefined && event.text !== "" && event.text !== null) {
-        weekday = event.text.toLowerCase()
-    } else {
-        weekday = weekdays[d.getDay()]
+    // ISO week date weeks start on monday
+    // so correct the day number
+    var dayNr   = (this.getDay() + 6) % 7;
+
+    // ISO 8601 states that week 1 is the week
+    // with the first thursday of that year.
+    // Set the target date to the thursday in the target week
+    target.setDate(target.getDate() - dayNr + 3);
+
+    // Store the millisecond value of the target date
+    var firstThursday = target.valueOf();
+
+    // Set the target to the first thursday of the year
+    // First set the target to january first
+    target.setMonth(0, 1);
+    // Not a thursday? Correct the date to the next thursday
+    if (target.getDay() != 4) {
+      target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
     }
 
-    var attachment = {
-        "attachments": [
-        {
-            "fallback": "IS24 canteen menu for " + weekday,
-            "image_url": "https://s3-eu-west-1.amazonaws.com/sodexo-slack/" + weekday + ".png"
-        }
-        ],
-        "response_type": "in_channel",
-        "text": "Canteen menu for " + weekday + " (brought to you by S.H.I.E.L.D.)"
-    }
+    // The weeknumber is the number of weeks between the
+    // first thursday of the year and the thursday in the target week
+    return 1 + Math.ceil((firstThursday - target) / 604800000); // 604800000 = 7 * 24 * 3600 * 1000
+  }
+  var d = new Date()
+  var weekdays = new Array(7)
+  weekdays[0]=  "sunday"
+  weekdays[1] = "monday"
+  weekdays[2] = "tuesday"
+  weekdays[3] = "wednesday"
+  weekdays[4] = "thursday"
+  weekdays[5] = "friday"
+  weekdays[6] = "saturday"
 
-    var invalidWeekday = {
-        "response_type": "in_channel",
-        "text": event.user_name + " fails at weekdays. '" + weekday + "' is not a valid weekday, please ask your colleagues about valid values."
-    }
+  var weekday = ""
+  if(event.text !== undefined && event.text !== "" && event.text !== null) {
+    weekday = event.text.toLowerCase()
+  } else {
+    weekday = weekdays[d.getDay()]
+  }
 
-    var spammer = {
-        "response_type": "ephemeral",
-        "text": "Please don't use the sodexo command in #general."
-    }
+  var attachment = {
+    "attachments": [
+      {
+        "fallback": "IS24 canteen menu for " + weekday,
+        "image_url": "https://s3-eu-west-1.amazonaws.com/sodexo-slack/" + d.getWeek() + "_" + weekday + ".png"
+      }
+    ],
+    "response_type": "in_channel",
+    "text": "Canteen menu for " + weekday + " (brought to you by S.H.I.E.L.D.)"
+  }
 
-    if(event.channel_name === "general"){
-        context.succeed(spammer)
-    }else if(weekdays.indexOf(weekday) >= 0){
-        context.succeed(attachment)
-    } else context.succeed(invalidWeekday)
+  var invalidWeekday = {
+    "response_type": "in_channel",
+    "text": event.user_name + " fails at weekdays. '" + weekday + "' is not a valid weekday, please ask your colleagues about valid values."
+  }
+
+  var spammer = {
+    "response_type": "ephemeral",
+    "text": "Please don't use the sodexo command in #general."
+  }
+
+  if(event.channel_name === "general"){
+    context.succeed(spammer)
+  }else if(weekdays.indexOf(weekday) >= 0){
+    context.succeed(attachment)
+  } else context.succeed(invalidWeekday)
 }
