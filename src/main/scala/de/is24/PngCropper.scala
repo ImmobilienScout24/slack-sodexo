81eeac3aad1ec48f130e9f018187c529c88c2c7e
Stefan Meiwald <stefan.meiwald@immobilienscout24.de>
2016-02-02 17:51:52 +0100

    Extended Tablerecognition in png cropper

--- src/main/scala/de/is24/PngCropper.scala
+++ src/main/scala/de/is24/PngCropper.scala
@@ -1,25 +1,40 @@
 package de.is24
 
+import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
 
 import org.apache.pdfbox.util.ImageIOUtil
 
+import scala.collection.immutable.IndexedSeq
+
 object PngCropper {
   val weekdays = Seq("monday", "tuesday", "wednesday", "thursday", "friday")
   val xOffset = 1226
   val yOffset = 1354
-  val tileWidth = 641
   val tileHeight = 1392
+  val stripeHeight: Int = 5
 
   def extractWeekdays(img: BufferedImage)(implicit logger: SodexoLogger): Map[String, Array[Byte]] = {
+    val orangeLines: IndexedSeq[Int] = (0 until img.getWidth - 1).flatMap {
+      case x: Int =>
+        val pixels = img.getRGB(x, img.getHeight / 2, 1, stripeHeight, null, 0, 1)
+        val nextPixels = img.getRGB(x + 1, img.getHeight / 2, 1, stripeHeight, null, 0, 1)
+        if (pixels.toSeq.forall(isOrange) && !nextPixels.toSeq.forall(isOrange)) Option(x)
+        else None
+    }
+
     logger.log("Extracting weekdays from image")
-    val offsets = (0 to 4).map { factor => xOffset + factor * tileWidth }
+    val offsets = orangeLines.drop(1).zip(orangeLines.drop(2))
     weekdays.zip(offsets).map {
-      case (day, offset) =>
+      case (day, (offset, offsetEnd)) =>
         val outputStream: ByteArrayOutputStream = new ByteArrayOutputStream()
-        ImageIOUtil.writeImage(img.getSubimage(offset, yOffset, tileWidth, tileHeight), "png", outputStream, ImageConstants.RESOLUTION, 1.0f)
+        ImageIOUtil.writeImage(img.getSubimage(offset, yOffset, offsetEnd - offset, tileHeight), "png", outputStream, ImageConstants.RESOLUTION, 1.0f)
         s"$day.png" -> outputStream.toByteArray
     }.toMap
+  }
+
+  def isOrange(rgb: Int): Boolean = {
+    rgb == new Color(247, 150, 70).getRGB
   }
 }
